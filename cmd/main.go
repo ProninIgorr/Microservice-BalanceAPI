b1fdccb02603b3c2f29fbddfb8b1bb39853bd5b7
Igor Pronin <goldenoronin1999@gmail.com>
2023-02-17 16:52:30 -0800

    add main

---
+++ cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	balance "github.com/ProninIgorr/MicroserviceBalance-Api/pkg"
+	"github.com/ProninIgorr/MicroserviceBalance-Api/pkg/handler"
+	"github.com/ProninIgorr/MicroserviceBalance-Api/pkg/repository"
+	"github.com/ProninIgorr/MicroserviceBalance-Api/pkg/service"
+	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os"
+)
+
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Username: os.Getenv("DB_USERNAME"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(balance.Server)
+
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		logrus.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
